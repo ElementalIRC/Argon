b532c13d2108e9f9f4b79fa81fa7fb1a2009820c
Archer <code.archer70@gmail.com>
2018-10-22 11:40:17 -0700

    Messages component.

--- src/App.vue
+++ src/App.vue
@@ -42,22 +42,16 @@
             </ul>
         </div>
     </div>
-    <div id="content">
-        <Message nick="TehCraw" poster="Gwenwyfar" timestamp="17:44:56" message="Out for dinner." />
-        <Message nick="TehCraw" poster="TehCraw" timestamp="17:45:23" message="See ya later, Gwenwyfar." />
-        <Message nick="TehCraw" poster="Oldiesmann" timestamp="17:52:42" message="Oldiesmann slaps TehCraw with an oven mit." />
-        <Message nick="TehCraw" poster="TehCraw" timestamp="17:52:23" message="I'll oven your mitt." />
-        <Message nick="TehCraw" poster="Oldiesmann" timestamp="17:53:35" message="lol" />
-    </div>
+    <Messages />
   </div>
 </template>
 
 <script>
-import Message from './components/Message.vue';
+import Messages from './components/Messages.vue';
 
 export default {
     components: {
-        Message: Message
+        Messages: Messages
     },
     mounted() {
     }
---
+++ src/components/Messages.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="content">
+        <Message nick="TehCraw" poster="Gwenwyfar" timestamp="17:44:56" message="Out for dinner." />
+        <Message nick="TehCraw" poster="TehCraw" timestamp="17:45:23" message="See ya later, Gwenwyfar." />
+        <Message nick="TehCraw" poster="Oldiesmann" timestamp="17:52:42" message="Oldiesmann slaps TehCraw with an oven mit." />
+        <Message nick="TehCraw" poster="TehCraw" timestamp="17:52:23" message="I'll oven your mitt." />
+        <Message nick="TehCraw" poster="Oldiesmann" timestamp="17:53:35" message="lol" />
+    </div>
+</template>
+
+<script>
+import md5 from 'md5';
+import Message from './Message.vue';
+
+export default {
+    name: 'Messages',
+    components: {
+        Message: Message
+    },
+    data() {
+        return {
+            messages: []
+        }
+    },
+    mounted() {
+    }
+}
+</script>
+
+<style lang="scss">
+</style>
